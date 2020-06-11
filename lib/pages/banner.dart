ac47db98fef3dc009d1f0f3f6776c36ac17a756b
winnietherpooh <winnietherpooh@163.com>
2020-06-11 22:00:51 +0800

    修复banner

--- lib/pages/banner.dart
+++ lib/pages/banner.dart
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-10 21:22:18
+ * @LastEditTime: 2020-06-11 22:00:19
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \yssqc\lib\pages\banner.dart
+ */ 
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
@@ -55,7 +63,26 @@ class _BannerPageState extends State<BannerPage> {
         ),
       );
     }else{
-      return Text('加载中');
+      return Container(
+        height: sySetWidth(330),
+        child: new Swiper(
+          itemBuilder: (BuildContext context, int index) {
+            return new Image.asset(
+              'images/banner.png',
+              fit: BoxFit.cover,
+            );
+          },
+          autoplay: true,
+          itemCount: 1,
+          scrollDirection: Axis.horizontal,
+          // pagination: new SwiperPagination(alignment: Alignment.centerRight),
+          //control: new SwiperControl(),
+          onTap: (v){   //点击事件
+           ExtendedNavigator.of(context).pushNamed(Routes.loginPageRoute);
+
+          },
+        ),
+      );
     }
   }
 }
