19df97c3f73c2a99ffd3f60236cb5db7be473393
winnietherpooh <winnietherpooh@163.com>
2020-06-13 15:14:39 +0800

    1111

--- lib/global.dart
+++ lib/global.dart
@@ -15,8 +15,8 @@ class Global {
     token: null,
   );
 
-  //static String domain = 'http://test.yousuanshi.com/reckoner/';
-  static String domain = 'http://192.168.0.136/';
+  static String domain = 'http://test.yousuanshi.com/reckoner/';
+  //static String domain = 'http://192.168.0.136/';
   static int  companyId = 18;
   /// 发布渠道
   static String channel = "xiaomi";
